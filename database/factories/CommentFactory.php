a7e997ce0b5ba80860df9411ec53dac43e78eba2
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2023-01-30 19:37:03 +0100

    update: prueba

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'body' => $this->faker->text,
+            'user_id' => $this->faker->numberBetween(1, 100),
+            'post_id' => $this->faker->numberBetween(1, 100),
+        ];
+    }
+}
---
+++ database/factories/CommunityFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CommunityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'creator' => $this->faker->name,
+        ];
+    }
+}
